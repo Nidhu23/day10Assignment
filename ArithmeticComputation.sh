b1fe4bd456f9e96824f5b5896e2243a3b65ad56a
nidhu23 <nidhunanik@gmail.com>
2020-06-02 11:03:37 +0530

    Modified: Added results to a dictionary

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -1,7 +1,8 @@
 #!/bin/bash  -x
 
 read -p "ENTER 3 Numbers " a b c
-$(($a+$b*$c))
-$(($a*$b+$c))
-$(($c+$a/$b))
-$(($a%$b+$c))
+declare -A operations
+operations[sum1]=$(($a+$b*$c))
+operations[sum2]=$(($a*$b+$c))
+operations[sum3]=$(($c+$a/$b))
+operations[sum4]=$(($a%$b+$c))
