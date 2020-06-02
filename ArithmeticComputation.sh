925a12cede7e67ac3b028be96265491a4664d4d9
nidhu23 <nidhunanik@gmail.com>
2020-06-02 16:38:27 +0530

    Modified: sorting in ascending order

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -1,18 +1,18 @@
 #!/bin/bash  -x
-
+#$c+$a/$b
 read -p "ENTER 3 Numbers " a b c
 declare -A operations
 cnt=0
 operations[sum1]=$(($a+$b*$c))
 operations[sum2]=$(($a*$b+$c))
-operations[sum3]=$(($c+$a/$b))
+operations[sum3]=`echo "scale=2;$c+$a/$b;" | bc `
 operations[sum4]=$(($a%$b+$c))
 for i in ${!operations[@]}
 do
         a[((cnt++))]=${operations[$i]}
 done
 echo ${operations[@]}
-c=$(printf "%d\n" ${a[@]} | sort -r -g )
+c=$(printf "%f\n" ${a[@]} | sort -r -g )
 echo "Values sorted in descending order " ${c[@]}
-b=$(printf "%d\n"  ${a[@]} | sort -n )
+b=$(printf "%f\n"  ${a[@]} | sort -n )
 echo "Values sorted in ascending order " ${b[@]}
